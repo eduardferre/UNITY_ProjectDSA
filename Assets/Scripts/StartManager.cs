39290b9702a4ca73bdf77fdaed04d81407be76e7
catymosteiro <73163835+catymosteiro@users.noreply.github.com>
2022-01-18 21:36:16 +0100

    Start Game

---
+++ Assets/Scripts/StartManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
